152511876c928e2e7e6a86cccbce490948cd1a72
AbdelkaderKHEZOUR <khabdelkaderdjilani@gmail.com>
2023-05-14 13:17:25 +0200

    fix build issues

--- app/api/auth/schema.ts
+++ app/api/auth/schema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
 
-export const nameSchema = z.string().min(3).max(5)
+export const nameSchema = z.string().min(3).max(100)
 export const emailSchema = z.string().email()
-export const passwordSchema = z.string().min(4).max(24)
+export const passwordSchema = z.string().min(4).max(100)
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import clsx from 'clsx'
 import Context from './context'
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import NavControls from './components/nav/controls'
 import { getServerAuth } from './components/auth'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Explore',
@@ -19,7 +19,7 @@ export default async function RootLayout(props: any) {
   const s = await getServerAuth();
   return (
     <html lang="en">
-      <body className={clsx(inter.className, "flex items-center w-screen h-full")}>
+      <body className={clsx("flex items-center w-screen h-full")}>
         <Context {...props}>
           <main className="h-full flex flex-col items-center justify-between md:px-24 pt-32 w-full">
             <div className='max-w-5xl w-full h-full'>
